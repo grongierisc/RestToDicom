53d52946a572e03fd38e5c05105e12a50d511ed4
Guillaume Rongier <guillaume.rongier@intersystems.com>
2019-03-14 12:02:40 +0100

    Correctif remove.sh

--- remove.sh
+++ remove.sh
@@ -18,22 +18,20 @@ password=$2
 namespace="RESTTODICOM"
 csppath="/api/resttodicom/v1"
 
-tClassImportDir=$DIR/install
-
 irissession $instanceName -U %SYS <<EOF 
 SuperUser
 $password
 
-Set NsEiste = ##class(Config.Namespaces).Exists("\$namespace")
-do:(NsEiste) ##class(Config.Namespaces).Delete("\$namespace")
-do:(NsEiste) ##class(%Library.EnsembleMgr).DisableNamespace("\$namespace")
+Set NsEiste = ##class(Config.Namespaces).Exists("$namespace")
+do:(NsEiste) ##class(Config.Namespaces).Delete("$namespace")
+do:(NsEiste) ##class(%Library.EnsembleMgr).DisableNamespace("$namespace")
 
-set CspExiste = ##Class(Security.Applications).Exists("\$csppath")
-do:(CspExiste) ##Class(Security.Applications).Delete("\$csppath")
+set CspExiste = ##Class(Security.Applications).Exists("$csppath")
+do:(CspExiste) ##Class(Security.Applications).Delete("$csppath")
 
-set DbExiste = ##class(Config.Databases).Exists("\$namespace")
-set Directory = ##class(Config.Databases).GetDirectory("\$namespace")
-do:(DbExiste) ##class(Config.Databases).Delete("\$namespace")
+set DbExiste = ##class(Config.Databases).Exists("$namespace")
+set Directory = ##class(Config.Databases).GetDirectory("$namespace")
+do:(DbExiste) ##class(Config.Databases).Delete("$namespace")
 do ##class(SYS.Database).DeleteDatabase(Directory)
 
 halt
